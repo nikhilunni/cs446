7b7e08304ebf3e1c5d12e16236c5987203cd477a
Nikhil Unni <nunni2@illinois.edu>
2014-09-25 04:47:32 -0500

    CAN'T BELIEVE IT WORKS

---
+++ hw2/decision-trees/src/BalloonTester.java
@@ -0,0 +1,34 @@
+package cs446.homework2;
+
+import java.io.File;
+import java.io.FileReader;
+
+import weka.classifiers.Evaluation;
+import weka.core.Instances;
+import cs446.weka.classifiers.trees.Id3;
+
+public class BalloonTester {
+
+    public static void main(String[] args) throws Exception {
+
+	if (args.length != 1) {
+	    System.err.println("Usage: WekaTester arff-file");
+	    System.exit(-1);
+	}
+
+	// Load the data
+	Instances data = new Instances(new FileReader(new File(args[0])));
+
+	// The last attribute is the class label
+	data.setClassIndex(data.numAttributes() - 1);
+	Id3 classifier = new Id3();
+	classifier.setMaxDepth(-1);
+
+	// Train
+	classifier.buildClassifier(data);
+
+	// Print the classfier
+	System.out.println(classifier);
+	System.out.println();
+    }
+}
