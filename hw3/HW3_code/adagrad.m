9d88cc77de3ba588a2b9d039f86377c7dfc86efc
Nikhil Unni <nunni2@illinois.edu>
2014-10-06 22:23:28 -0500

    something new idk

--- hw3/HW3_code/adagrad.m
+++ hw3/HW3_code/adagrad.m
@@ -1,9 +1,8 @@
 function [w_out,theta_out] = adagrad(x,y,eta)
     [m,n] = size(x); %m = #examples, n = #features
     w = [ones(1,n),-1*n];
-%    G = zeros(1,n+1);
-    for j = 1:20
-       G = zeros(1,n+1); 
+    G = zeros(1,n+1);
+    for j = 1:20       
        for i = 1:m
          if (dot(w(1:n),x(i,:))+w(n+1)) * y(i) <= 1
             g = [-y(i)*x(i,:), -y(i)];           
